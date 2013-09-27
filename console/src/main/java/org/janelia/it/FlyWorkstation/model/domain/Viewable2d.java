b8b8cd5fb3a2879c277286b0d7b948f01dbcb1f7
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-09-27 19:35:55 +0000

    implementation of mask/chan for reference channel
    Former-commit-id: a7cdf91b2ffed17d07d97ee6f50a020492789a78

--- console/src/main/java/org/janelia/it/FlyWorkstation/model/domain/Viewable2d.java
+++
@@ -1,7 +0,0 @@
-package org.janelia.it.FlyWorkstation.model.domain;
-
-public interface Viewable2d {
-
-    public String get2dImageFilepath();
-    
-}
--- console/src/main/java/org/janelia/it/FlyWorkstation/model/domain/Viewable3d.java
+++
@@ -1,8 +0,0 @@
-package org.janelia.it.FlyWorkstation.model.domain;
-
-public interface Viewable3d {
-
-    public String get3dImageFilepath();
-    
-    public String getFast3dImageFilepath();
-}
