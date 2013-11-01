f4dca88a1c0f1f03433b0939fa192d287e56f2c8
Les Foster <fosterl@janelia.hhmi.org>
2013-11-01 19:28:36 +0000

    No longer using glVertexPointer and glTexCoordPointer.  Dropped in favor of glVertexAttribPointer and shader change to use.
    Former-commit-id: eb3dece41b17af8ec0f757310a42a708fe7d7148

--- console/src/main/java/org/janelia/it/FlyWorkstation/gui/static_view/shader/RGBExcludableVtx.glsl
+++
@@ -1,9 +0,0 @@
-// vertex shader to support dynamic selection of presented colors.
-#version 120
-
-void main(void)
-{
-    gl_FrontColor = gl_Color;
-    gl_TexCoord[0] = gl_MultiTexCoord0;
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-}
