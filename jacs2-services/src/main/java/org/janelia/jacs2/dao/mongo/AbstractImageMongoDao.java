c798f4f8fb2fd97e0ff4376a3c143274cd72392b
Cristian Goina <goinac@janelia.hhmi.org>
2017-05-10 16:42:24 -0400

    made AbstractImageMongoDao abstract so that it doesn't get instantiated since it messes up the entity type
    Former-commit-id: 4273f45d9cf1584a2148ec987eefcf460276083f

--- jacs2-services/src/main/java/org/janelia/jacs2/dao/mongo/AbstractImageMongoDao.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/dao/mongo/AbstractImageMongoDao.java
@@ -7,12 +7,9 @@ import org.janelia.jacs2.dao.ImageDao;
 import org.janelia.it.jacs.model.domain.sample.Image;
 import org.janelia.jacs2.dao.mongo.utils.TimebasedIdentifierGenerator;
 
-import javax.inject.Inject;
-
 import static com.mongodb.client.model.Filters.eq;
 
-public class AbstractImageMongoDao<T extends Image> extends AbstractDomainObjectDao<T> implements ImageDao<T> {
-    @Inject
+public abstract class AbstractImageMongoDao<T extends Image> extends AbstractDomainObjectDao<T> implements ImageDao<T> {
     public AbstractImageMongoDao(MongoDatabase mongoDatabase, TimebasedIdentifierGenerator idGenerator, ObjectMapperFactory objectMapperFactory) {
         super(mongoDatabase, idGenerator, objectMapperFactory);
     }
