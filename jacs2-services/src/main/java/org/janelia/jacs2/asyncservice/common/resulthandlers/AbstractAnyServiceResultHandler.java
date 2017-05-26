f722db22b3fe0138162262a9b6dec07a602dc0d5
Cristian Goina <goinac@janelia.hhmi.org>
2017-05-26 15:05:31 -0400

    when saving the result don't convert it to json tree
    Former-commit-id: ffd7f73939bec3bcc4c39024bf94b473add2407d

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/common/resulthandlers/AbstractAnyServiceResultHandler.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/common/resulthandlers/AbstractAnyServiceResultHandler.java
@@ -1,6 +1,5 @@
 package org.janelia.jacs2.asyncservice.common.resulthandlers;
 
-import org.janelia.jacs2.asyncservice.common.ServiceDataUtils;
 import org.janelia.jacs2.asyncservice.common.ServiceResultHandler;
 import org.janelia.jacs2.model.jacsservice.JacsServiceData;
 
@@ -8,7 +7,7 @@ public abstract class AbstractAnyServiceResultHandler<T> implements ServiceResul
 
     @Override
     public void updateServiceDataResult(JacsServiceData jacsServiceData, T result) {
-        jacsServiceData.setSerializableResult(ServiceDataUtils.anyToSerializableObject(result));
+        jacsServiceData.setSerializableResult(result);
     }
 
 }
