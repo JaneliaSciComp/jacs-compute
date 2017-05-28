b704f8af7544e3543ae9c67d41795f46b2cefbd8
Cristian Goina <goinac@janelia.hhmi.org>
2017-05-28 00:41:09 -0400

    removed qualifier
    Former-commit-id: 29158648e5d9833a7334f200c578d12db6b294f3

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/common/mdc/MdcContext.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/common/mdc/MdcContext.java
@@ -1,6 +1,5 @@
 package org.janelia.jacs2.asyncservice.common.mdc;
 
-import javax.inject.Qualifier;
 import javax.interceptor.InterceptorBinding;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
@@ -9,7 +8,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Inherited
-@Qualifier
 @InterceptorBinding
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
