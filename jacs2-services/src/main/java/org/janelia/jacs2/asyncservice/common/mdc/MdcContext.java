e9577c77e97254ecd647303d62da224e9868efc8
Cristian Goina <goinac@janelia.hhmi.org>
2017-05-27 22:27:56 -0400

    trying stereotype
    Former-commit-id: 9dec5532685c8573b2603af77deb6c4eea99b634

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/common/mdc/MdcContext.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/common/mdc/MdcContext.java
@@ -1,5 +1,7 @@
 package org.janelia.jacs2.asyncservice.common.mdc;
 
+import javax.enterprise.inject.Stereotype;
+import javax.inject.Named;
 import javax.interceptor.InterceptorBinding;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
@@ -8,6 +10,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Inherited
+@Named
+@Stereotype
 @InterceptorBinding
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
