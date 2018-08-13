9b1cc4892cc49392b7ce243c095014d25046fe20
Cristian Goina <goinac@janelia.hhmi.org>
2018-08-13 18:30:31 -0400

    pull requires a singularity URL
    Former-commit-id: dcde71fb6c452f96d2e3f720738d5953959d6848

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/containerizedservices/PullAndRunSingularityContainerProcessor.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/containerizedservices/PullAndRunSingularityContainerProcessor.java
@@ -58,7 +58,7 @@ public class PullAndRunSingularityContainerProcessor extends AbstractServiceProc
         return pullContainerProcessor.process(new ServiceExecutionContext.Builder(jacsServiceData)
                         .description("Pull container image " + args.containerLocation)
                         .build(),
-                new ServiceArg("-containerLocation", getContainerLocation(args)))
+                new ServiceArg("-containerLocation", args.containerLocation))
                 .thenCompose(containerImageResult -> createExpandedItemsComputation(containerImageResult, args, expandArguments(args), jacsServiceData))
                 ;
     }
