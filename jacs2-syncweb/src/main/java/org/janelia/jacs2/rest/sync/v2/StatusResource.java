c743df091510e6b286bf613764cf94644a3c9235
Cristian Goina <goinac@hhmi.org>
2018-10-12 20:09:06 -0400

    status resource
    Former-commit-id: eec4923e086653b91148901ed2b911c6b13f1000

---
+++ jacs2-syncweb/src/main/java/org/janelia/jacs2/rest/sync/v2/StatusResource.java
@@ -0,0 +1,20 @@
+package org.janelia.jacs2.rest.sync.v2;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+@Produces("application/json")
+@Path("/")
+public class StatusResource {
+    @GET
+    @Path("{node-id}")
+    public Response getStatus() {
+        return Response
+                .ok("OK")
+                .build();
+    }
+}
