ae8c5c525f7352621cd5ff38e6fe57d7e7ab70a2
Cristian Goina <goinac@hhmi.org>
2018-11-05 10:43:59 -0500

    partial commit
    Former-commit-id: 51ade672e77c902abb05fdf5c6ee10d49c4bd4f4

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/neuronservices/SWCService.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/neuronservices/SWCService.java
@@ -1,30 +1,62 @@
 package org.janelia.jacs2.asyncservice.neuronservices;
 
+import org.janelia.jacs2.cdi.qualifier.PropertyValue;
+import org.janelia.model.access.domain.dao.TmSampleDao;
 import org.janelia.model.access.domain.dao.TmWorkspaceDao;
+import org.janelia.model.domain.tiledMicroscope.TmSample;
 import org.janelia.model.domain.tiledMicroscope.TmWorkspace;
+import org.janelia.model.rendering.RenderedVolumeLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class SWCService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SWCService.class);
+
+    private final TmSampleDao tmSampleDao;
     private final TmWorkspaceDao tmWorkspaceDao;
+    @Inject
+    private RenderedVolumeLoader renderedVolumeLoader;
+    private final Path defaultSWCLocation;
 
     @Inject
-    public SWCService(TmWorkspaceDao tmWorkspaceDao) {
+    public SWCService(TmSampleDao tmSampleDao,
+                      TmWorkspaceDao tmWorkspaceDao,
+                      @PropertyValue(name = "service.swcImport.DefaultLocation") String defaultSWCLocation) {
+        this.tmSampleDao = tmSampleDao;
         this.tmWorkspaceDao = tmWorkspaceDao;
+        this.defaultSWCLocation = Paths.get(defaultSWCLocation);
     }
 
-    public TmWorkspace importSWVFolder(String swcFolderName, Long sampleId, String workspaceName, String workspaceOwnerKey, List<String> accessibleBy) {
-        TmWorkspace tmWorkspace = tmWorkspaceDao.createTmWorkspace(workspaceOwnerKey, createWorkspace(sampleId, workspaceName, accessibleBy));
+    public TmWorkspace importSWVFolder(String swcFolderName, Long sampleId, String workspaceName, String workspaceOwnerKey, List<String> accessUsers) {
+        TmSample tmSample = tmSampleDao.findByIdAndSubjectKey(sampleId, workspaceOwnerKey);
+        if (tmSample == null) {
+            LOG.error("Sample {} either does not exist or user {} has no access to it", sampleId, workspaceOwnerKey);
+            throw new IllegalArgumentException("Sample " + sampleId + " either does not exist or is not accessible");
+        }
+        TmWorkspace tmWorkspace = tmWorkspaceDao.createTmWorkspace(workspaceOwnerKey, createWorkspace(swcFolderName, sampleId, workspaceName, accessUsers));
+        renderedVolumeLoader.loadVolume(Paths.get(tmSample.getFilepath()));
         // TODO !!!!
         return tmWorkspace;
     }
 
-    private TmWorkspace createWorkspace(Long sampleId, String workspaceName, List<String> accessibleBy) {
+    private TmWorkspace createWorkspace(String swcFolderName, Long sampleId, String workspaceName, List<String> accessUsers) {
+        Path swcPath = Paths.get(swcFolderName);
+        Path swcBasePath;
+        if (swcPath.isAbsolute()) {
+            swcBasePath = swcPath;
+        } else {
+            swcBasePath = defaultSWCLocation.resolve(swcPath);
+        }
         TmWorkspace tmWorkspace = new TmWorkspace(workspaceName, sampleId);
-        tmWorkspace.getReaders().addAll(accessibleBy);
-        tmWorkspace.getWriters().addAll(accessibleBy);
+        tmWorkspace.setOriginalSWCPath(swcBasePath.toString());
+        tmWorkspace.getReaders().addAll(accessUsers);
+        tmWorkspace.getWriters().addAll(accessUsers);
         return tmWorkspace;
     }
 }
