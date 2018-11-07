9dc521881bb7bcfb6bc1423db13cd680060b0e81
Cristian Goina <goinac@hhmi.org>
2018-11-07 15:15:09 -0500

    added applicaiton id
    Former-commit-id: ab2ecce58e59450ccc17f73d0566d6472922e56a

--- jacs2-commonweb/src/main/java/org/janelia/jacs2/filter/CORSResponseFilter.java
+++ jacs2-commonweb/src/main/java/org/janelia/jacs2/filter/CORSResponseFilter.java
@@ -7,12 +7,10 @@ import java.io.IOException;
 
 
 public class CORSResponseFilter implements ContainerResponseFilter {
-
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext resp) throws IOException {
         resp.getHeaders().add("Access-Control-Allow-Origin","*");
         resp.getHeaders().add("Access-Control-Allow-Methods","GET,POST,PUT");
-        resp.getHeaders().add("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        resp.getHeaders().add("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization, Application-Id");
     }
-
 }
