cc19b1e895ab31611e797c7f4e4b065084a5c570
Cristian Goina <goinac@hhmi.org>
2018-11-19 17:50:02 -0500

    still working on serializing the class attribute
    Former-commit-id: d227934eb6123d2339e57c100a0c5fd856636874

--- jacs2-commonweb/src/main/java/org/janelia/jacs2/app/JAXAppConfig.java
+++ jacs2-commonweb/src/main/java/org/janelia/jacs2/app/JAXAppConfig.java
@@ -4,27 +4,29 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.janelia.jacs2.cdi.ObjectMapperFactory;
 import org.janelia.jacs2.filter.AuthFilter;
 import org.janelia.jacs2.filter.CORSResponseFilter;
 import org.janelia.jacs2.provider.ObjectMapperResolver;
 import org.janelia.jacs2.rest.InvalidArgumentRequestHandler;
 import org.janelia.jacs2.rest.InvalidJsonRequestHandler;
 import org.janelia.jacs2.rest.v2.AppVersionResource;
+import org.janelia.model.NumberSerializerModule;
 
 public class JAXAppConfig extends ResourceConfig {
     JAXAppConfig(String... packageNames) {
-        packages(true, packageNames);
+        register(new JacksonJaxbJsonProvider(ObjectMapperFactory.instance()
+                .newObjectMapper().registerModule(new NumberSerializerModule()), JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS)
+        );
         registerClasses(
-                JacksonJaxbJsonProvider.class,
                 InvalidArgumentRequestHandler.class,
                 InvalidJsonRequestHandler.class,
                 ObjectMapperResolver.class,
                 // Putting the multipart package in the param above does not work. We need to be explicit with the classname.
-                JacksonFeature.class,
                 MultiPartFeature.class,
                 AppVersionResource.class,
                 CORSResponseFilter.class,
                 AuthFilter.class);
-
+        packages(true, packageNames);
     }
 }
