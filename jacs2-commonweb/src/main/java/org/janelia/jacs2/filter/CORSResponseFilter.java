9e6cfbd9ec21b91b420e0c4d3342c39d76756a87
Cristian Goina <goinac@hhmi.org>
2018-12-14 11:36:25 -0500

    added delete to allowed methods
    Former-commit-id: 54aedc1d22e34c49fdfb8300a3b6959265991cd0

--- jacs2-commonweb/src/main/java/org/janelia/jacs2/filter/CORSResponseFilter.java
+++ jacs2-commonweb/src/main/java/org/janelia/jacs2/filter/CORSResponseFilter.java
@@ -5,12 +5,11 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import java.io.IOException;
 
-
 public class CORSResponseFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext resp) throws IOException {
         resp.getHeaders().add("Access-Control-Allow-Origin","*");
-        resp.getHeaders().add("Access-Control-Allow-Methods","GET,POST,PUT");
+        resp.getHeaders().add("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
         resp.getHeaders().add("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization, Application-Id, Content-Disposition");
     }
 }
