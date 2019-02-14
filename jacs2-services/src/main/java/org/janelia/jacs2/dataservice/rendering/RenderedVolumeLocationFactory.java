b21b03a7c6254ffa6972ffe6a529cdde93e70a90
Cristian Goina <goinac@hhmi.org>
2019-02-13 23:40:32 -0500

    only use jade volume location

--- jacs2-services/src/main/java/org/janelia/jacs2/dataservice/rendering/RenderedVolumeLocationFactory.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/dataservice/rendering/RenderedVolumeLocationFactory.java
@@ -4,14 +4,11 @@ import com.google.common.base.Preconditions;
 import org.apache.commons.lang3.StringUtils;
 import org.janelia.jacs2.cdi.qualifier.PropertyValue;
 import org.janelia.jacs2.dataservice.storage.StorageService;
-import org.janelia.rendering.FileBasedRenderedVolumeLocation;
 import org.janelia.rendering.JADEBasedRenderedVolumeLocation;
 import org.janelia.rendering.RenderedVolumeLocation;
 import org.janelia.rendering.utils.HttpClientProvider;
 
 import javax.inject.Inject;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class RenderedVolumeLocationFactory {
     private final StorageService storageService;
@@ -29,14 +26,10 @@ public class RenderedVolumeLocationFactory {
 
     public RenderedVolumeLocation getVolumeLocation(String samplePath, String subjectKey, String authToken) {
         Preconditions.checkArgument(StringUtils.isNotBlank(samplePath));
-        if (Files.exists(Paths.get(samplePath))) {
-            return new FileBasedRenderedVolumeLocation(Paths.get(samplePath));
-        } else {
-            return storageService.lookupStorage(null, null, null, samplePath, subjectKey, authToken)
-                    .map(dsInfo -> new JADEBasedRenderedVolumeLocation(dsInfo.getDataStorageURI(), authToken, storageServiceApiKey, httpClientProvider))
-                    .orElseThrow(() -> new IllegalArgumentException("No volume location could be created for sample at " + samplePath))
-                    ;
-        }
+        return storageService.lookupStorage(null, null, null, samplePath, subjectKey, authToken)
+                .map(dsInfo -> new JADEBasedRenderedVolumeLocation(dsInfo.getDataStorageURI(), authToken, storageServiceApiKey, httpClientProvider))
+                .orElseThrow(() -> new IllegalArgumentException("No volume location could be created for sample at " + samplePath))
+                ;
     }
 
 }
