91121deceb73881f9ca87bc1926b6da4a6414f5d
Cristian Goina <goinac@hhmi.org>
2019-05-13 21:11:08 -0400

    use messageconnection instead of connectionmanager

--- jacs2-services/src/main/java/org/janelia/jacs2/cdi/MessagingProducer.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/cdi/MessagingProducer.java
@@ -3,7 +3,7 @@ package org.janelia.jacs2.cdi;
 import org.apache.commons.lang3.StringUtils;
 import org.janelia.jacs2.cdi.qualifier.PropertyValue;
 import org.janelia.messaging.core.MessageSender;
-import org.janelia.messaging.core.impl.ConnectionManager;
+import org.janelia.messaging.core.impl.MessageConnection;
 import org.janelia.messaging.core.impl.MessageSenderImpl;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,25 +14,26 @@ public class MessagingProducer {
 
     @ApplicationScoped
     @Produces
-    public ConnectionManager createConnectionManager() {
-        return ConnectionManager.getInstance();
+    public MessageConnection createIndexingMessageConnection(@PropertyValue(name = "Messaging.Server") String messagingServer,
+                                                             @PropertyValue(name = "Messaging.User") String messagingUser,
+                                                             @PropertyValue(name = "Messaging.Password") String messagingPassword) {
+        if (StringUtils.isNotBlank(messagingServer)) {
+            MessageConnection messageConnection = new MessageConnection();
+            messageConnection.openConnection(messagingServer, messagingUser, messagingPassword, 1);
+            return messageConnection;
+        } else {
+            return null;
+        }
     }
 
     @ApplicationScoped
     @Produces
-    public MessageSender createIndexingMessageSender(ConnectionManager connectionManager,
-                                                     @PropertyValue(name = "Messaging.Server") String messagingServer,
-                                                     @PropertyValue(name = "Messaging.User") String messagingUser,
-                                                     @PropertyValue(name = "Messaging.Password") String messagingPassword,
+    public MessageSender createIndexingMessageSender(MessageConnection messageConnection,
                                                      @PropertyValue(name = "Messaging.AsyncIndexingExchange") String asyncIndexingExchange,
                                                      @PropertyValue(name = "Messaging.AsyncIndexingRoutingKey") String asyncIndexingRoutingKey) {
-        if (StringUtils.isNotBlank(messagingServer)) {
-            MessageSender messageSender = new MessageSenderImpl(connectionManager);
-            messageSender.connect(messagingServer, messagingUser, messagingPassword, asyncIndexingExchange, asyncIndexingRoutingKey);
-            return messageSender;
-        } else {
-            return null;
-        }
+        MessageSender messageSender = new MessageSenderImpl(messageConnection);
+        messageSender.connectTo(asyncIndexingExchange, asyncIndexingRoutingKey);
+        return messageSender;
     }
 
 }
