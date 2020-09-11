cfcf517e6c9e1beba51df8328ec2d7c495073719
Cristian Goina <goinac@hhmi.org>
2020-09-11 12:33:28 -0400

    implemented delete operation

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/ColorDepthLibraryValidator.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/ColorDepthLibraryValidator.java
@@ -1,37 +1,47 @@
 package org.janelia.jacs2.asyncservice.imagesearch;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import com.beust.jcommander.Parameter;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import org.janelia.jacs2.asyncservice.common.*;
+
+import org.janelia.jacs2.asyncservice.common.AbstractServiceProcessor;
+import org.janelia.jacs2.asyncservice.common.JacsServiceResult;
+import org.janelia.jacs2.asyncservice.common.ServiceArgs;
+import org.janelia.jacs2.asyncservice.common.ServiceComputation;
+import org.janelia.jacs2.asyncservice.common.ServiceComputationFactory;
+import org.janelia.jacs2.asyncservice.utils.FileUtils;
 import org.janelia.jacs2.cdi.qualifier.PropertyValue;
 import org.janelia.jacs2.dataservice.persistence.JacsServiceDataPersistence;
-import org.janelia.model.access.cdi.AsyncIndex;
 import org.janelia.model.access.dao.JacsNotificationDao;
 import org.janelia.model.access.dao.LegacyDomainDao;
-import org.janelia.model.access.domain.dao.ColorDepthImageDao;
 import org.janelia.model.domain.ChanSpecUtils;
 import org.janelia.model.domain.gui.cdmip.ColorDepthFileComponents;
 import org.janelia.model.domain.gui.cdmip.ColorDepthImage;
-import org.janelia.model.domain.sample.ObjectiveSample;
 import org.janelia.model.domain.sample.Sample;
-import org.janelia.model.domain.sample.SampleAlignmentResult;
-import org.janelia.model.domain.sample.SamplePipelineRun;
 import org.janelia.model.service.JacsNotification;
 import org.janelia.model.service.JacsServiceData;
 import org.janelia.model.service.JacsServiceLifecycleStage;
 import org.janelia.model.service.ServiceMetaData;
 import org.slf4j.Logger;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.util.*;
-
 /**
  * Walk existing color depth libraries and validate them against existing samples.
- *
+ * <p>
  * Delete rogue images and flag anything that looks suspicious.
- *
+ * <p>
  * This service takes care of corner cases such as when a sample's gender is changed, the alignment
  * is recomputed, and a new CDM image is placed in a different folder, but the old one is not cleaned
  * up. This service should run afterwards and clean things up.
@@ -59,20 +69,17 @@ public class ColorDepthLibraryValidator extends AbstractServiceProcessor<Void> {
     }
 
     private final LegacyDomainDao legacyDomainDao;
-    private final ColorDepthImageDao colorDepthImageDao;
     private final JacsNotificationDao jacsNotificationDao;
 
     @Inject
     ColorDepthLibraryValidator(ServiceComputationFactory computationFactory,
-                                  JacsServiceDataPersistence jacsServiceDataPersistence,
-                                  @PropertyValue(name = "service.DefaultWorkingDir") String defaultWorkingDir,
-                                  LegacyDomainDao legacyDomainDao,
-                                  @AsyncIndex ColorDepthImageDao colorDepthImageDao,
-                                  JacsNotificationDao jacsNotificationDao,
-                                  Logger logger) {
+                               JacsServiceDataPersistence jacsServiceDataPersistence,
+                               @PropertyValue(name = "service.DefaultWorkingDir") String defaultWorkingDir,
+                               LegacyDomainDao legacyDomainDao,
+                               JacsNotificationDao jacsNotificationDao,
+                               Logger logger) {
         super(computationFactory, jacsServiceDataPersistence, defaultWorkingDir, logger);
         this.legacyDomainDao = legacyDomainDao;
-        this.colorDepthImageDao = colorDepthImageDao;
         this.jacsNotificationDao = jacsNotificationDao;
     }
 
@@ -116,9 +123,9 @@ public class ColorDepthLibraryValidator extends AbstractServiceProcessor<Void> {
         long totalNumDeleted = 0;
 
         Map<String, Map<String, Integer>> counts = legacyDomainDao.getColorDepthCounts();
-        TreeSet<String> libraries = new TreeSet<>(counts.keySet());
+        Set<String> libraries = new TreeSet<>(counts.keySet());
 
-        for(String libraryIdentifier : libraries) {
+        for (String libraryIdentifier : libraries) {
 
             if (args.library != null && !args.library.equals(libraryIdentifier)) {
                 continue;
@@ -127,12 +134,12 @@ public class ColorDepthLibraryValidator extends AbstractServiceProcessor<Void> {
             totalNumLibrariesProcessed++;
 
             Map<Long, Sample> samples = new HashMap<>();
-            for(Sample sample : legacyDomainDao.getSamplesByDataSet(null, libraryIdentifier)) {
+            for (Sample sample : legacyDomainDao.getSamplesByDataSet(null, libraryIdentifier)) {
                 samples.put(sample.getId(), sample);
             }
             logger.trace("  Found {} samples associated with {}", samples.size(), libraryIdentifier);
 
-            for(String alignmentSpace : new TreeSet<>(counts.get(libraryIdentifier).keySet())) {
+            for (String alignmentSpace : new TreeSet<>(counts.get(libraryIdentifier).keySet())) {
 
                 // Ignore older alignments that were deleted from samples
                 if (!alignmentSpace.startsWith("JRC2018")) continue;
@@ -148,54 +155,36 @@ public class ColorDepthLibraryValidator extends AbstractServiceProcessor<Void> {
 
                 // Get all the CDMs for this library/alignmentSpace and group them by sample
                 List<ColorDepthImage> images = legacyDomainDao.getColorDepthImages(null, libraryIdentifier, alignmentSpace);
-                Multimap<Long, ColorDepthImage> imagesBySample = HashMultimap.create();
-                for(ColorDepthImage image : images) {
-                    if (image.getSampleRef()==null) {
-                        // This is generally fine. Imported CDMs don't have a sampleRef, e.g. everything from flyem
-                        // TODO: it would be nice to distinguish between imported and "native" libraries so that we could run this check.
-                        continue;
-                    }
-                    imagesBySample.put(image.getSampleRef().getTargetId(), image);
-                }
-
+                Multimap<Long, ColorDepthImage> imagesBySample = partitionColorDepthMIPsBySample(images);
                 logger.info("    Found {} images mapping to {} samples", images.size(), imagesBySample.keySet().size());
 
                 List<ColorDepthImage> toDelete = new ArrayList<>();
 
-                for(Long sampleId : imagesBySample.keySet()) {
+                for (Long sampleId : imagesBySample.keySet()) {
                     Sample sample = samples.get(sampleId);
 
-                    if (sample==null) {
-                        // This probably means that we're coming at the sample from a constructed library, and not from
-                        // its original data-set-linked library. That's fine, and we'll probably get to it later.
-                        // But it could also mean that the sample is gone or is inaccessible,
-                        // and this service doesn't currently address that edge case.
+                    if (sample == null) {
+                        // this is an error because a sampleRef in one of the mips is invalid
+                        logger.error("No sample found for {}", sampleId);
                         continue;
                     }
 
-                    // Create set of valid CDMs in this sample
-                    Set<String> cdms = new HashSet<>();
-                    for(ObjectiveSample objectiveSample : sample.getObjectiveSamples()) {
-                        for(SamplePipelineRun run : objectiveSample.getPipelineRuns()) {
-                            for(SampleAlignmentResult alignment : run.getAlignmentResults()) {
-                                if (alignment.getAlignmentSpace()!=null && alignment.getAlignmentSpace().equals(alignmentSpace)) {
-                                    List<Integer> signals = ChanSpecUtils.getSignalChannelIndexList(alignment.getChannelSpec());
-                                    for(Integer signalIndex : signals) {
-                                        String cdm = objectiveSample.getObjective() + DELIM + alignment.getAlignmentSpace()
-                                                + DELIM + alignment.getAnatomicalArea() + DELIM + (signalIndex+1);
-                                        cdms.add(cdm);
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    // Collect all valid CDM names for the current sample
+                    Set<String> validSampleCDMs = sample.getObjectiveSamples().stream()
+                            .flatMap(objectiveSample -> objectiveSample.getPipelineRuns().stream())
+                            .flatMap(pipelineRun -> pipelineRun.getAlignmentResults().stream())
+                            .filter(alignment -> alignmentSpace.equals(alignment.getAlignmentSpace()))
+                            .flatMap(alignment -> ChanSpecUtils.getSignalChannelIndexList(alignment.getChannelSpec()).stream()
+                                    .map(signalIndex -> alignment.getObjective() + DELIM + alignment.getAlignmentSpace()
+                                            + DELIM + alignment.getAnatomicalArea() + DELIM + (signalIndex + 1)))
+                            .collect(Collectors.toSet());
 
                     // Compare all the CDMs actually found with the valid CDMs
                     for (ColorDepthImage image : imagesBySample.get(sampleId)) {
                         ColorDepthFileComponents file = ColorDepthFileComponents.fromFilepath(image.getFilepath());
                         String cdm = file.getObjective() + DELIM + file.getAlignmentSpace()
                                 + DELIM + file.getAnatomicalArea() + DELIM + file.getChannelNumber();
-                        if (!cdms.contains(cdm)) {
+                        if (!validSampleCDMs.contains(cdm)) {
                             logger.info("    Will delete rogue CDM: {}", image.getFilepath());
                             toDelete.add(image);
                         }
@@ -214,8 +203,8 @@ public class ColorDepthLibraryValidator extends AbstractServiceProcessor<Void> {
                         logger.warn("    Percentage of images to delete exceeds safety limit. Resubmit with force=true to continue.");
                         continue;
                     }
-                    for (ColorDepthImage image : toDelete) {
-                        if (!args.dryRun) {
+                    if (!args.dryRun) {
+                        for (ColorDepthImage image : toDelete) {
                             if (deleteColorDepthImage(image)) {
                                 numDeleted++;
                             }
@@ -234,9 +223,27 @@ public class ColorDepthLibraryValidator extends AbstractServiceProcessor<Void> {
         logger.info("Processed {} images in total, and deleted {} rogue CDMs.", totalNumImagesProcessed, totalNumDeleted);
     }
 
-    private boolean deleteColorDepthImage(ColorDepthImage image) {
-        // TODO: delete file on disk
-        // TODO: delete object in database
-        return true; // success
+    private Multimap<Long, ColorDepthImage> partitionColorDepthMIPsBySample(List<ColorDepthImage> mips) {
+        Multimap<Long, ColorDepthImage> mipsBySample = HashMultimap.create();
+        for (ColorDepthImage mip : mips) {
+            if (mip.getSampleRef() == null) {
+                // This is generally fine. Imported CDMs don't have a sampleRef, e.g. everything from flyem
+                // TODO: it would be nice to distinguish between imported and "native" libraries so that we could run this check.
+                continue;
+            }
+            mipsBySample.put(mip.getSampleRef().getTargetId(), mip);
+        }
+        return mipsBySample;
+    }
+
+    private boolean deleteColorDepthImage(ColorDepthImage colorDepthImage) {
+        try {
+            FileUtils.deletePath(Paths.get(colorDepthImage.getFilepath()));
+            legacyDomainDao.deleteDomainObject(null, colorDepthImage.getClass(), colorDepthImage.getId());
+            return true;
+        } catch (IOException e) {
+            logger.error("Error deleting {} for {}", colorDepthImage.getFilepath(), colorDepthImage);
+            return false;
+        }
     }
 }
