45c9887c997ee96034f8fed09e205b9fcecb5010
Cristian Goina <goinac@hhmi.org>
2020-09-23 10:05:25 -0400

    collect results

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/JavaProcessColorDepthFileSearch.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/JavaProcessColorDepthFileSearch.java
@@ -1,9 +1,12 @@
 package org.janelia.jacs2.asyncservice.imagesearch;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
@@ -18,6 +21,7 @@ import org.janelia.jacs2.asyncservice.common.ServiceArgs;
 import org.janelia.jacs2.asyncservice.common.ServiceComputationFactory;
 import org.janelia.jacs2.asyncservice.common.ServiceResultHandler;
 import org.janelia.jacs2.asyncservice.common.resulthandlers.AbstractFileListServiceResultHandler;
+import org.janelia.jacs2.asyncservice.utils.FileUtils;
 import org.janelia.jacs2.asyncservice.utils.ScriptWriter;
 import org.janelia.jacs2.cdi.qualifier.ApplicationProperties;
 import org.janelia.jacs2.cdi.qualifier.StrPropertyValue;
@@ -75,7 +79,10 @@ public class JavaProcessColorDepthFileSearch extends AbstractExeBasedServiceProc
 
             @Override
             public List<File> collectResult(JacsServiceData jacsServiceData) {
-                throw new UnsupportedOperationException();
+                ColorDepthSearchArgs args = getArgs(jacsServiceData);
+                return FileUtils.lookupFiles(Paths.get(args.cdMatchesDir), 1, "glob:**/*")
+                        .map(Path::toFile)
+                        .collect(Collectors.toList());
             }
         };
     }
