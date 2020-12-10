d0c03456510db2891d87d1105b7a0131263f1211
Cristian Goina <goinac@hhmi.org>
2020-12-10 16:11:41 -0500

    one more LSM endpoint

--- jacs2-syncweb/src/main/java/org/janelia/jacs2/rest/sync/v2/dataresources/LSMImageInfoResource.java
+++ jacs2-syncweb/src/main/java/org/janelia/jacs2/rest/sync/v2/dataresources/LSMImageInfoResource.java
@@ -1,12 +1,12 @@
 package org.janelia.jacs2.rest.sync.v2.dataresources;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -21,6 +21,7 @@ import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import io.swagger.annotations.SecurityDefinition;
 import io.swagger.annotations.SwaggerDefinition;
+import org.apache.commons.lang3.StringUtils;
 import org.janelia.jacs2.auth.annotations.RequireAuthentication;
 import org.janelia.jacs2.rest.ErrorResponse;
 import org.janelia.model.access.domain.dao.LSMImageDao;
@@ -52,6 +53,47 @@ public class LSMImageInfoResource {
     @Inject
     private LSMImageDao lsmImageDao;
 
+    @ApiOperation(value = "Gets lsm image stack by name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully found and returned image", response = Response.class),
+            @ApiResponse(code = 404, message = "LSM image not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error while looking up image")
+    })
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("lsmstack/images")
+    public Response getLSMImageStackByMatchingName(@ApiParam @QueryParam("name") String name,
+                                                   @ApiParam @QueryParam("includeNonSynced") Boolean includeNonSynced) {
+        LOG.trace("Start getLSMStackByName({})", name);
+        try {
+            if (StringUtils.isBlank(name)) {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity(new ErrorResponse("Invalid image name"))
+                        .build();
+            }
+            LSMImage lsmImage = findImagesByMatchingName(name).stream()
+                    .filter(lsm -> includeNonSynced != null && includeNonSynced || lsm.isLSMSageSynced())
+                    .findFirst()
+                    .orElse(null);
+            if (lsmImage == null) {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity(new ErrorResponse("No image " + name + "found"))
+                        .build();
+            } else {
+                return Response
+                        .ok(lsmImage)
+                        .build();
+            }
+        } catch (Exception e) {
+            LOG.error("Error occurred getting image {}", name, e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new ErrorResponse("Error retrieving image " + name))
+                    .build();
+        } finally {
+            LOG.trace("Finished getLSMStackByName({})", name);
+        }
+    }
+
     @ApiOperation(value = "Gets lsm stack by name")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully found and returned image", response = Response.class),
@@ -60,17 +102,22 @@ public class LSMImageInfoResource {
     })
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("lsmstack/name/{name}")
-    public Response getLSMStackByName(@ApiParam @PathParam("name") String imageName,
+    @Path("lsmstack/name")
+    public Response getLSMStackByName(@ApiParam @QueryParam("name") String name,
                                       @ApiParam @QueryParam("fuzzyMatch") Boolean fuzzyMatch,
                                       @ApiParam @QueryParam("includeNonSynced") Boolean includeNonSynced) {
-        LOG.trace("Start getLSMStackByName({})", imageName);
+        LOG.trace("Start getLSMStackByName({})", name);
         try {
+            if (StringUtils.isBlank(name)) {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity(new ErrorResponse("Invalid image name"))
+                        .build();
+            }
             List<LSMImage> matchingImages;
             if (fuzzyMatch != null && fuzzyMatch) {
-                matchingImages = lsmImageDao.findEntitiesByExactName(imageName);
+                matchingImages = findImagesByExactName(name);
             } else {
-                matchingImages = lsmImageDao.findEntitiesWithMatchingName(imageName);
+                matchingImages = findImagesByMatchingName(name);
             }
             LSMImage lsmImage = matchingImages.stream()
                     .filter(lsm -> includeNonSynced != null && includeNonSynced || lsm.isLSMSageSynced())
@@ -78,7 +125,7 @@ public class LSMImageInfoResource {
                     .orElse(null);
             if (lsmImage == null) {
                 return Response.status(Response.Status.NOT_FOUND)
-                        .entity(new ErrorResponse("No image " + imageName + "found"))
+                        .entity(new ErrorResponse("No image " + name + "found"))
                         .build();
             } else {
                 return Response
@@ -86,13 +133,30 @@ public class LSMImageInfoResource {
                         .build();
             }
         } catch (Exception e) {
-            LOG.error("Error occurred getting image {}", imageName, e);
+            LOG.error("Error occurred getting image {}", name, e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(new ErrorResponse("Error retrieving image " + imageName))
+                    .entity(new ErrorResponse("Error retrieving image " + name))
                     .build();
         } finally {
-            LOG.trace("Finished getLSMStackByName({})", imageName);
+            LOG.trace("Finished getLSMStackByName({})", name);
         }
+    }
+
+    private List<LSMImage> findImagesByMatchingName(String name) {
+        String imageName;
+        String archivedImageName;
+        if (StringUtils.endsWith(name, ".bz2")) {
+            imageName = StringUtils.removeEnd(name, ".bz2");
+            archivedImageName = name;
+        } else {
+            imageName = name;
+            archivedImageName = name + ".bz2";
+        }
+        return lsmImageDao.findEntitiesMatchingAnyGivenName(Arrays.asList(imageName, archivedImageName));
+    }
+
+    private List<LSMImage> findImagesByExactName(String name) {
+        return lsmImageDao.findEntitiesByExactName(name);
     }
 
 }
