e4d108a6bac5a02ae18426830fc34b910ce9a29e
Cristian Goina <goinac@hhmi.org>
2020-12-16 15:43:40 -0500

    renamed library partition size flag

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/JavaProcessColorDepthFileSearch.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/JavaProcessColorDepthFileSearch.java
@@ -151,7 +151,7 @@ public class JavaProcessColorDepthFileSearch extends AbstractExeBasedServiceProc
             externalScriptWriter.addArg("--with-grad-scores");
         }
         if (args.partitionSize != null && args.partitionSize > 0) {
-            externalScriptWriter.addArgs("--libraryPartitionSize", args.partitionSize.toString());
+            externalScriptWriter.addArgs("--processingPartitionSize", args.partitionSize.toString());
         }
         if (ncores > 0) {
             externalScriptWriter.addArgs("--cdsConcurrency", String.valueOf(2 * ncores - 1));
