0964b2fa8712eabc458f85465f8666e636c4ce69
Cristian Goina <goinac@hhmi.org>
2020-12-17 12:49:45 -0500

    collect results 1 level deeper

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/ColorDepthFileSearchProcessingUtils.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/imagesearch/ColorDepthFileSearchProcessingUtils.java
@@ -12,7 +12,8 @@ import org.janelia.jacs2.asyncservice.utils.FileUtils;
 class ColorDepthFileSearchProcessingUtils {
 
     static List<File> collectResults(ColorDepthSearchArgs args) {
-        return FileUtils.lookupFiles(Paths.get(args.cdMatchesDir), 1, "glob:**/*")
+        // search 2 levels deep because each subprocess may put the results in its own subdirectory
+        return FileUtils.lookupFiles(Paths.get(args.cdMatchesDir), 2, "glob:**/*")
                 .filter(Files::isRegularFile)
                 .map(Path::toFile)
                 .collect(Collectors.toList());
