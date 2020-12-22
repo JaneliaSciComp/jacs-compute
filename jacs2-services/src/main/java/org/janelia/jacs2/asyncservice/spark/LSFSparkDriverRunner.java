6b38298c49930ccc6c2759454683a6b8e67d93cf
Cristian Goina <goinac@hhmi.org>
2020-12-22 14:24:21 -0500

    leave the executor cores same as worker cores

--- jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/spark/LSFSparkDriverRunner.java
+++ jacs2-services/src/main/java/org/janelia/jacs2/asyncservice/spark/LSFSparkDriverRunner.java
@@ -58,7 +58,7 @@ class LSFSparkDriverRunner implements SparkDriverRunner<LSFJobSparkApp> {
         }
         driverOptionsBuilder.add("--master", sparkClusterInfo.getMasterURI());
         int nCoresPerSparkWorker = SparkAppResourceHelper.getCoresPerSparkWorker(sparkAppResources);
-        driverOptionsBuilder.add("--conf").add(SparkLauncher.EXECUTOR_CORES + "=" + (nCoresPerSparkWorker * 2));
+        driverOptionsBuilder.add("--conf").add(SparkLauncher.EXECUTOR_CORES + "=" + nCoresPerSparkWorker);
         int sparkMemPerCoreInGB = SparkAppResourceHelper.getSparkWorkerMemoryPerCoreInGB(sparkAppResources);
         if (sparkMemPerCoreInGB > 0) {
             driverOptionsBuilder.add("--executor-memory").add(nCoresPerSparkWorker * sparkMemPerCoreInGB + "g");
@@ -72,7 +72,7 @@ class LSFSparkDriverRunner implements SparkDriverRunner<LSFJobSparkApp> {
         }
         int nSparkWorkers = SparkAppResourceHelper.getSparkWorkers(sparkAppResources);
         int appDefinedParallelism = SparkAppResourceHelper.getSparkParallelism(sparkAppResources);
-        int sparkParallelism = appDefinedParallelism != 0 ? appDefinedParallelism : nSparkWorkers * nCoresPerSparkWorker * 2;
+        int sparkParallelism = appDefinedParallelism != 0 ? appDefinedParallelism : nSparkWorkers * nCoresPerSparkWorker;
         if (sparkParallelism > 0) {
             // The default (4MB) open cost consolidates files into tiny partitions regardless of number of cores.
             // By forcing this parameter to zero, we can specify the exact parallelism we want.
