486e6fdbb3baf8ac0d7456410fca5d78376ad964
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2022-02-07 15:24:30 -0500

    reordered parameters in publishedImage API

--- jacs2-syncweb/src/main/java/org/janelia/jacs2/rest/sync/v2/dataresources/PublishedImageResource.java
+++ jacs2-syncweb/src/main/java/org/janelia/jacs2/rest/sync/v2/dataresources/PublishedImageResource.java
@@ -54,18 +54,21 @@ public class PublishedImageResource {
     })
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/image/{slideCode}/{alignmentSpace}/{objective}")
-    public Response getImage(@ApiParam @PathParam("slideCode") String slideCode,
-                             @ApiParam @PathParam("alignmentSpace") String alignmentSpace,
-                             @ApiParam @PathParam("objective") String objective) {
-        LOG.trace("Start getImage({}, {}, {})", slideCode, alignmentSpace, objective);
+    //@Path("/image/{slideCode}/{alignmentSpace}/{objective}")
+    @Path("/image/{alignmentSpace}/{objective}/{slideCode}")
+    public Response getImage(@ApiParam @PathParam("alignmentSpace") String alignmentSpace,
+                             @ApiParam @PathParam("objective") String objective,
+                             @ApiParam @PathParam("slideCode") String slideCode) {
+        LOG.trace("Start getImage({}, {}, {})", alignmentSpace, objective, slideCode);
         try {
+            // minor note: the argument order is different in the DAO than in the REST API, because
+            //  I was (still am?) uncertain as to what's best and have been changing my mind
             PublishedImage image = publishedImageDao.getImage(slideCode, alignmentSpace, objective);
             if (image == null) {
-                LOG.warn("Could not find image for {}, {}, {}", slideCode, alignmentSpace, objective);
+                LOG.warn("Could not find image for {}, {}, {}", alignmentSpace, objective, slideCode);
                 return Response.status(Response.Status.BAD_REQUEST)
-                        .entity(new ErrorResponse("No image found for " + slideCode + ", "
-                                + alignmentSpace + ", " + objective))
+                        .entity(new ErrorResponse("No image found for "
+                                + alignmentSpace + ", " + objective + ", " + slideCode))
                         .build();
             }
 
@@ -77,13 +80,13 @@ public class PublishedImageResource {
                     .ok(new GenericEntity<List<PublishedImage>>(publishedImages){})
                     .build();
         } catch (Exception e) {
-            LOG.error("Error occurred getting published image for {}, {}, {}", slideCode, alignmentSpace, objective, e);
+            LOG.error("Error occurred getting published image for {}, {}, {}", alignmentSpace, objective, slideCode, e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(new ErrorResponse("Error retrieving published image for " + slideCode + ", "
-                            + alignmentSpace + ", " + objective))
+                    .entity(new ErrorResponse("Error retrieving published image for "
+                            + alignmentSpace + ", " + objective+ ", " + slideCode))
                     .build();
         } finally {
-            LOG.trace("Finished getImage({}, {}, {})", slideCode, alignmentSpace, objective);
+            LOG.trace("Finished getImage({}, {}, {})", alignmentSpace, objective, slideCode);
         }
     }
 }
