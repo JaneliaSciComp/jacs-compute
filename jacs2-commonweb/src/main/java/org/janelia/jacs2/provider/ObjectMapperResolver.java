6e6224b9b37883bd314b24b3f05f92d4d5cfb567
Cristian Goina <goinac@hhmi.org>
2022-09-09 12:10:45 -0400

    ignore SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS to be able to serialize EMBody with dataset information

--- jacs2-commonweb/src/main/java/org/janelia/jacs2/provider/ObjectMapperResolver.java
+++ jacs2-commonweb/src/main/java/org/janelia/jacs2/provider/ObjectMapperResolver.java
@@ -1,6 +1,8 @@
 package org.janelia.jacs2.provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import org.janelia.jacs2.cdi.ObjectMapperFactory;
 import org.janelia.model.NumberSerializerModule;
 
@@ -14,7 +16,8 @@ public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
     @Inject
     public ObjectMapperResolver(ObjectMapperFactory mapperFactory) {
         this.mapper = mapperFactory.newObjectMapper()
-                .registerModule(new NumberSerializerModule());
+                .registerModule(new NumberSerializerModule())
+                .configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false);
     }
 
     @Override
