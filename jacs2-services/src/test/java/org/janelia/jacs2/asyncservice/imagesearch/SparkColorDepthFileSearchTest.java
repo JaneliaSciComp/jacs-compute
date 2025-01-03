629e75ed200c52316d48a2c44c16c36abc2c3018
Cristian Goina <goinac@hhmi.org>
2025-01-03 16:16:48 -0500

    fixed unit test for spark color depth file search

--- jacs2-services/src/test/java/org/janelia/jacs2/asyncservice/imagesearch/SparkColorDepthFileSearchTest.java
+++ jacs2-services/src/test/java/org/janelia/jacs2/asyncservice/imagesearch/SparkColorDepthFileSearchTest.java
@@ -3,35 +3,28 @@ package org.janelia.jacs2.asyncservice.imagesearch;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
-import com.google.common.collect.ImmutableList;
-
 import org.janelia.jacs2.asyncservice.common.ComputationTestHelper;
-import org.janelia.jacs2.asyncservice.common.JacsServiceFolder;
 import org.janelia.jacs2.asyncservice.common.JacsServiceResult;
 import org.janelia.jacs2.asyncservice.common.ServiceArg;
 import org.janelia.jacs2.asyncservice.common.ServiceArgMatcher;
 import org.janelia.jacs2.asyncservice.common.ServiceComputation;
 import org.janelia.jacs2.asyncservice.common.ServiceComputationFactory;
 import org.janelia.jacs2.asyncservice.common.ServiceExecutionContext;
-import org.janelia.jacs2.asyncservice.common.ServiceResultHandler;
 import org.janelia.jacs2.asyncservice.spark.SparkAppProcessor;
-import org.janelia.jacs2.asyncservice.utils.FileUtils;
 import org.janelia.jacs2.dataservice.persistence.JacsServiceDataPersistence;
 import org.janelia.jacs2.testhelpers.ListArgMatcher;
 import org.janelia.model.service.JacsServiceData;
 import org.janelia.model.service.JacsServiceDataBuilder;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
 import org.slf4j.Logger;
 
 import static org.junit.Assert.fail;
@@ -111,57 +104,57 @@ public class SparkColorDepthFileSearchTest {
         Mockito.when(sparkAppProcessor.process(any(JacsServiceData.class)))
                 .then(invocation -> serviceComputationFactory.newCompletedComputation(new JacsServiceResult(testService)));
 
-//        PowerMockito.mockStatic(Files.class);
-        Mockito.when(Files.createDirectories(any(Path.class))).then((Answer<Path>) invocation -> invocation.getArgument(0));
-        Mockito.when(Files.find(any(Path.class), anyInt(), any(BiPredicate.class))).then(invocation -> {
-            Path root = invocation.getArgument(0);
-            return Stream.of(root.resolve("f1_results.json"));
-        });
+        try (MockedStatic<Files> mockedFiles = Mockito.mockStatic(Files.class)) {
+            mockedFiles.when(() -> Files.find(any(Path.class), anyInt(), any(BiPredicate.class))).then(invocation -> {
+                Path root = invocation.getArgument(0);
+                return Stream.of(root.resolve("f1_results.json"));
+            });
 
-        ServiceComputation<JacsServiceResult<List<File>>> colorDepthFileSearchComputation = sparkColorDepthFileSearch.process(testService);
+            ServiceComputation<JacsServiceResult<List<File>>> colorDepthFileSearchComputation = sparkColorDepthFileSearch.process(testService);
 
-        @SuppressWarnings("unchecked")
-        Consumer<JacsServiceResult<List<File>>> successful = mock(Consumer.class);
-        @SuppressWarnings("unchecked")
-        Consumer<Throwable> failure = mock(Consumer.class);
-        colorDepthFileSearchComputation
-                .thenApply(r -> {
-                    successful.accept(r);
-                    Mockito.verify(sparkAppProcessor).createServiceData(
-                            any(ServiceExecutionContext.class),
-                            argThat(new ListArgMatcher<>(
-                                    Arrays.asList(
-                                            new ServiceArgMatcher(new ServiceArg("-appName", "colordepthsearch")),
-                                            new ServiceArgMatcher(new ServiceArg("-appLocation", jarPath)),
-                                            new ServiceArgMatcher(new ServiceArg("-appEntryPoint", "org.janelia.colormipsearch.cmd.SparkMainEntry")),
-                                            new ServiceArgMatcher(new ServiceArg("-appArgs",
-                                                    "searchFromJSON," +
-                                                            "-m," + maskFiles + "," +
-                                                            "-i," + targetFiles + "," +
-                                                            "--maskThreshold," + maskThreshold + "," +
-                                                            "--dataThreshold," + targetThreshold + "," +
-                                                            "--pixColorFluctuation," + pixColorFluctuation + "," +
-                                                            "--xyShift," + xyShift + "," +
-                                                            "--mirrorMask," +
-                                                            "--pctPositivePixels," + pctPositivePixels + "," +
-                                                            "--negativeRadius," + negativeRadius + "," +
-                                                            "--with-grad-scores," +
-                                                            "--outputDir," + resultsDir))
-                                    )
-                            ))
-                    );
-                    return r;
-                })
-                .exceptionally(exc -> {
-                    failure.accept(exc);
-                    if (exc instanceof NullPointerException) {
-                        // this usually occurs if the mocks are not setup correctly
-                        fail("Check that the parameters for the mock invocation are as they are expected by the test setup: " + exc.toString());
-                    } else {
-                        fail(exc.toString());
-                    }
-                    return null;
-                });
+            @SuppressWarnings("unchecked")
+            Consumer<JacsServiceResult<List<File>>> successful = mock(Consumer.class);
+            @SuppressWarnings("unchecked")
+            Consumer<Throwable> failure = mock(Consumer.class);
+            colorDepthFileSearchComputation
+                    .thenApply(r -> {
+                        successful.accept(r);
+                        Mockito.verify(sparkAppProcessor).createServiceData(
+                                any(ServiceExecutionContext.class),
+                                argThat(new ListArgMatcher<>(
+                                        Arrays.asList(
+                                                new ServiceArgMatcher(new ServiceArg("-appName", "colordepthsearch")),
+                                                new ServiceArgMatcher(new ServiceArg("-appLocation", jarPath)),
+                                                new ServiceArgMatcher(new ServiceArg("-appEntryPoint", "org.janelia.colormipsearch.cmd.SparkMainEntry")),
+                                                new ServiceArgMatcher(new ServiceArg("-appArgs",
+                                                        "searchFromJSON," +
+                                                                "-m," + maskFiles + "," +
+                                                                "-i," + targetFiles + "," +
+                                                                "--maskThreshold," + maskThreshold + "," +
+                                                                "--dataThreshold," + targetThreshold + "," +
+                                                                "--pixColorFluctuation," + pixColorFluctuation + "," +
+                                                                "--xyShift," + xyShift + "," +
+                                                                "--mirrorMask," +
+                                                                "--pctPositivePixels," + pctPositivePixels + "," +
+                                                                "--negativeRadius," + negativeRadius + "," +
+                                                                "--with-grad-scores," +
+                                                                "--outputDir," + resultsDir))
+                                        )
+                                ))
+                        );
+                        return r;
+                    })
+                    .exceptionally(exc -> {
+                        failure.accept(exc);
+                        if (exc instanceof NullPointerException) {
+                            // this usually occurs if the mocks are not setup correctly
+                            fail("Check that the parameters for the mock invocation are as they are expected by the test setup: " + exc.toString());
+                        } else {
+                            fail(exc.toString());
+                        }
+                        return null;
+                    });
+        }
     }
 
     private JacsServiceData createTestServiceData(Number serviceId,
@@ -192,8 +185,7 @@ public class SparkColorDepthFileSearchTest {
                 .addArgs("-negativeRadius", negativeRadius)
                 .addArgs("-cdMatchesDir", resultsDir)
                 .addArgs("-numWorkers", numWorkers)
-                .addArgs("-withGradientScores")
-                ;
+                .addArgs("-withGradientScores");
         JacsServiceData testServiceData = testServiceDataBuilder
                 .setWorkspace(TEST_WORKSPACE)
                 .build();
